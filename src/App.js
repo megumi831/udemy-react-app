f64bfdf8676a4782c799813cac6f15d70cc40f6e
shitoumegumi <mg831mac@yahoo.co.jp>
2020-05-29 22:34:19 +0900

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <lavel htmlFor="bar">bar</lavel>
-        <input type="text" onClick={() => {console.log("Hi!")}}/>
-      </React.Fragment>
-    )
-    
-    
-  }
+const App = () => {
+  const profiles = [
+    { name: "Ossa", age: "30" },
+    { name: "Megu", age: "30" },
+    { name: "haruki", age: 1 }
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+    </div>
+  )
+  
 }
-
+const User = (props) => {
+  return <div>Hi,I am {props.name}, and {props.age} years old</div>
+}
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
+}
 
 export default App;
