b0a2d124f5c7bc7ff506002dc13de74495a2ad5d
shitoumegumi <mg831mac@yahoo.co.jp>
2020-05-31 04:36:40 +0900

    material-ui

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -12,6 +12,13 @@ class EventsShow extends Component {
     this.onSubmit = this.onSubmit.bind(this)
     this.onDeleteClick = this.onDeleteClick.bind(this)
   }
+
+componentDidMount() {
+  const { id } = this.props.match.params
+  if (id) this.props.getEvent(id)
+}
+
+
   renderField(field) {
     const { input, label, type, meta: { touched, error }} = field
 
@@ -30,12 +37,12 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
-    // await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
 
   render() {
-    const { handleSubmit, pristine, submitting } = this.props
+    const { handleSubmit, pristine, submitting, invalid } = this.props
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div>
@@ -45,7 +52,7 @@ class EventsShow extends Component {
           <Field label="Body" name="body" type="text" component={this.renderField} />
         </div>
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting} />
+          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
 
           <Link to="/">Cancel</Link>
           <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
@@ -64,8 +71,12 @@ const validate = values => {
   return errors
 }
 
+const mapStateToProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id]
+  return { initialValues: event, event }
+}
+const mapDispatchToProps = ({ deleteEvent, getEvent, putEvent })
 
-const mapDispatchToProps = ({ deleteEvent })
-export default connect(null, mapDispatchToProps)(
-  reduxForm({ validate, form: 'EventsShowForm' })(EventsShow)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  reduxForm({ validate, form: 'EventShowForm', enableReinitialize: true })(EventsShow)
 )
